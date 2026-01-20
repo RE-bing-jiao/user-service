263038fc19bdd9d0f6bdd8c7146a1074d1b4d550
RE-bing-jiao <nikalaschanka.eva@mail.ru>
2026-01-20 14:35:49 +0300

    fix: change constructor access modifier from public to private

--- src/main/java/com/example/internship/microservice/utils/LogMessages.java
+++ src/main/java/com/example/internship/microservice/utils/LogMessages.java
@@ -32,6 +32,6 @@ public class LogMessages {
     public static final String METHOD_START = "Starting execution of method: {}";
     public static final String METHOD_END = "Completed execution of method: {}";
 
-    public LogMessages() {
+    private LogMessages() {
     }
 }
